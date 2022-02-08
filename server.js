1e0c8bb558a24753ad3d6e8d89996ac1b51fefb7
Gerald Maier-Köck <GHEMaier@users.noreply.github.com>
2022-02-08 22:21:45 +0100

    20220208-222129

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http') 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'}) console.log ('responding')
-    res.end(`Hello readers!
+    res.end(`Hello FRIENDS!
     If you see this, then your Node.js server is running on AWS EC2!`)
 }).listen(80, () => {
     console.log ('server is up')
