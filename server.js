8bdd743424cd59850bf7e9ddab33d32564f019a6
Gerald Maier-Köck <GHEMaier@users.noreply.github.com>
2022-02-13 20:13:41 +0100

    20220213-201339

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = 3000;
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     console.log ('responding');
-    res.end(`Hello FRIENDS!
+    res.end(`Hello KATHI!
     If you see this, then your Node.js server is running on AWS EC2!`);
 }).listen(port, () => {
     console.log ('server is up');
